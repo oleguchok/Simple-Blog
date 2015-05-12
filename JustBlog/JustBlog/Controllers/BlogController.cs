beaf528b29043fd2eeef7f11c3508a4654542e42
oleguhcok <oleguchok@gmail.com>
2015-05-12 22:58:28 +0300

    Create blog controller and action posts

---
+++ JustBlog/JustBlog/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using JustBlog.Core.Repository;
+using JustBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JustBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public ViewResult Posts(int p = 1)
+        {
+            // pick latest 10 posts
+            var listViewModel = new ListViewModel(_blogRepository,p);
+
+            ViewBag.Title = "Latest Posts";
+
+            return View("List", listViewModel);
+        }
+    }
+}
