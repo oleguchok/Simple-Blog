f75fc53d902c80f05e0f87a176b0ed7fd145d934
oleguhcok <oleguchok@gmail.com>
2015-05-13 02:26:02 +0300

    Create an action to return posts belongs a particular category

--- JustBlog/JustBlog/Controllers/BlogController.cs
+++ JustBlog/JustBlog/Controllers/BlogController.cs
@@ -26,5 +26,17 @@ namespace JustBlog.Controllers
 
             return View("List", listViewModel);
         }
+
+        public ViewResult Category(string category, int p = 1)
+        {
+            var viewModel = new ListViewModel(_blogRepository, category, p);
+
+            if (viewModel.Category == null)
+                throw new HttpException(404, "Category not found");
+
+            ViewBag.Title = String.Format(@"Latest posts on category ""{0}""",
+                                viewModel.Category.Name);
+            return View("List", viewModel);
+        }
     }
 }
