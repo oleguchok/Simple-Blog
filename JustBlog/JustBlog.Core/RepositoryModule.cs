446bca6413e64100c4a7880e3ecb98a55dac35f1
oleguhcok <oleguchok@gmail.com>
2015-05-12 22:20:04 +0300

    Configure Ninject for JustBlog.Core project

---
+++ JustBlog/JustBlog.Core/RepositoryModule.cs
@@ -0,0 +1,45 @@
+﻿using NHibernate;
+using Ninject.Modules;
+using Ninject.Web.Common;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Cache;
+using JustBlog.Core.Objects;
+using NHibernate.Tool.hbm2ddl;
+using Ninject;
+
+namespace JustBlog.Core
+{
+    public class RepositoryModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ISessionFactory>()
+                .ToMethod
+                (
+                    e =>
+                        Fluently.Configure()
+                        .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c =>
+                            c.FromConnectionStringWithKey("JustBlogDbConnString")))
+                        .Cache(c => c.UseQueryCache().ProviderClass<HashtableCacheProvider>())
+                        .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Post>())
+                        .ExposeConfiguration(cfg => new SchemaExport(cfg).Execute(true, true, false))
+                        .BuildConfiguration()
+                        .BuildSessionFactory()
+                )
+                .InSingletonScope();
+
+            Bind<ISession>()
+                .ToMethod((ctx) => ctx.Kernel.Get<ISessionFactory>().OpenSession())
+                .InRequestScope();
+        }
+    }
+}
